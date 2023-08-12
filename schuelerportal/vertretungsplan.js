3e066332eda9cc8d422a43b347b6b50d65f7a4d6
Niemand333 <71368780+Niemand333@users.noreply.github.com>
2023-08-12 17:41:01 +0200

    test

--- schuelerportal/vertretungsplan.js
+++ schuelerportal/vertretungsplan.js
@@ -1,25 +1,34 @@
 function reload() {
     var cookie = document.getElementById("cookie").value;
+    console.log(cookie)
     if (cookie != "") {
-        document.cookie = "schuelerportal_session=" + cookie;
+        document.cookie = "schuelerportal_session=" + cookie + "; SameSite=None;"
+        //document.cookie = "schuelerportal_school=clagybam; path=/; SameSite=None; Secure;";
     }
-    else {
-        cookie = document.cookie.split("=")[1];
-    }
-
-    loadPlan(cookie);
+    
+    loadPlan();
 }
 
 function start() {
     reload();
 }
 
-function loadPlan(cookie) {
+function loadPlan() {
     fetch("https://api.schueler.schule-infoportal.de/clagybam/api/vertretungsplan", {
+        mode: "cors",
         method: 'GET',
+        headers: {
+            //'Access-Control-Allow-Origin'  : "localhost:5500",
+            //'Access-Control-Allow-Credentials' : 'true',
+            'Content-Type' : 'application/json',
+            'Cookie' : document.cookie
+        },
         credentials: 'include'
-      })
+    })
+    .then((response) => console.log(response));
+    /*
     .then((response) => response.json())
     .then((json) => console.log(json));
+    */
   
 }
